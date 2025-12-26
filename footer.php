d7984fb3818dd087391123043b33102e45ac0309
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-12-26 19:47:04 +0600

    kajkormo_job_portal

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div>
+    <h2></h2>
+    <div class="grid-foot">
+
+      <div>Contact Us<br>+8801909845700<br>Email<br>help@kajkormo.com<br>Address<br>Dhaka, Bangladesh</div>
+      <div>Our Resources<br>Tools<br>job<br>Help desk<br>Opportunities</div>
+      <div>About us<br>How it work<br>Our objectives<br></div>
+      <div class="">আমরা ক্লায়েন্টের জন্য একটি সাধারণ ক্ষেত্র প্রদান করি এবং ফ্রিল্যান্সার, যাতে তারা একে অপরের সাথে সহজেই যোগাযোগ করতে পারে। এতে সবার উপকার হয়।</div>
+
+    </div>
+    <div class="ftrr">
+    All rights researved to কাজকর্ম.
+    </div>
+  </div>
